28876ec6c0ca4e11d33f22c3860cfc03f15eb1eb
Murilo <murilopb@Murilo>
2023-05-30 18:35:04 -0300

    initial commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "expoexampleamplify25cbcd09": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "AppClientSecret": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "expoupload": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
