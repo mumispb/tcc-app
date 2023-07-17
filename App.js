b42d579b722077025379b0521458f9ddb7372a05
Murilo <murilo.pereira.botelho@gmail.com>
2023-07-17 01:30:32 -0300

    feat: ui improvements

--- App.js
+++ App.js
@@ -4,22 +4,45 @@ import Storage from "@aws-amplify/storage";
 import * as Clipboard from "expo-clipboard";
 import Constants from "expo-constants";
 import * as ImagePicker from "expo-image-picker";
-import { useEffect, useState } from "react";
-import { Button, Image, StyleSheet, Text, View } from "react-native";
+import { useEffect, useRef, useState } from "react";
+import {
+  Button,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import axios from "axios";
-
+import Ionicons from "@expo/vector-icons/Ionicons";
 import awsconfig from "./aws-exports";
+import { Animated, Easing } from "react-native";
+import Lottie from "lottie-react-native";
+// yellow box warnings - ignore them
+console.disableYellowBox = true;
 
 Amplify.configure(awsconfig);
+
+const PREDICTION_INITIAL_STATE = {
+  class: "",
+  confidence: null,
+};
 
 export default function App() {
   const [image, setImage] = useState(null);
   const [percentage, setPercentage] = useState(0);
   const [hasUploaded, setHasUploaded] = useState(false);
-  const [prediction, setPrediction] = useState({
-    cat: 0,
-    dog: 0,
-  });
+  const [prediction, setPrediction] = useState(PREDICTION_INITIAL_STATE);
+  const animationProgress = useRef(new Animated.Value(0));
+
+  useEffect(() => {
+    console.log("entrou", percentage);
+    Animated.timing(animationProgress.current, {
+      toValue: percentage / 100,
+      easing: Easing.linear,
+      useNativeDriver: false,
+    }).start();
+  }, [percentage]);
 
   useEffect(() => {
     (async () => {
@@ -31,7 +54,7 @@ export default function App() {
           cameraRollStatus.status !== "granted" ||
           cameraStatus.status !== "granted"
         ) {
-          alert("Sorry, we need these permissions to make this work!");
+          // alert("Sorry, we need these permissions to make this work!");
         }
       }
     })();
@@ -67,6 +90,7 @@ export default function App() {
         setPercentage(0);
         const img = await fetchImageFromUri(pickerResult.uri);
         const uploadUrl = await uploadImage("demo.jpg", img);
+        console.log("upload deu certo");
         predict();
         setHasUploaded(true);
         downloadImage(uploadUrl);
@@ -122,18 +146,16 @@ export default function App() {
   };
 
   const predict = async () => {
-    setPrediction({
-      cat: 0,
-      dog: 0,
-    });
+    setPrediction(PREDICTION_INITIAL_STATE);
     try {
-      const response = await axios.get("http://192.168.86.210:5000/students");
+      const response = await axios.get("http://192.168.86.210:5000/evaluate");
+      console.log({ response: response.data });
       setPrediction({
-        cat: response.data.cat,
-        dog: response.data.dog,
+        class: response.data.class,
+        confidence: response.data.confidence,
       });
-      console.log({ response });
     } catch (error) {
+      console.log("Request failed");
       console.log(error);
       console.error(error);
     }
@@ -141,40 +163,156 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>AWS Storage Upload Demo</Text>
-      {percentage !== 0 && <Text style={styles.percentage}>{percentage}%</Text>}
+      {/* <Text style={styles.title}>AWS Storage Upload Demo</Text> */}
+      <Text
+        style={{
+          fontSize: 24,
+          fontWeight: "bold",
+          marginBottom: 20,
+          textAlign: "center",
+        }}
+      >
+        Classificador de Patologias Vegetais
+      </Text>
+      <Text
+        style={{
+          fontSize: 16,
+          marginBottom: 20,
+          textAlign: "center",
+        }}
+      >
+        Para utilizar, selecione uma imagem de uma folha de uma planta com
+        alguma patologia de sua galeria ou tire uma nova foto.
+      </Text>
+      {percentage !== 0 && !hasUploaded && (
+        <>
+          <Lottie
+            source={require("./assets/animation_lk6by1dw.json")}
+            progress={animationProgress.current}
+            loop={false}
+            style={{
+              height: 100,
+            }}
+          />
+          {/* <Text style={styles.percentage}>Upload: {percentage}%</Text> */}
+        </>
+      )}
 
-      {image && (
+      {image && hasUploaded && (
+        <View
+          style={{
+            width: "100%",
+            height: 250,
+            marginBottom: 20,
+            borderRadius: 8,
+          }}
+        >
+          {/* <Text style={styles.result} onPress={copyToClipboard}> */}
+          <Image
+            source={{ uri: image }}
+            style={{
+              borderRadius: 16,
+              flex: 1,
+            }}
+            resizeMode="contain"
+          />
+          {/* </Text> */}
+          {/* <Text style={styles.info}>Long press to copy the image url</Text> */}
+        </View>
+      )}
+      {hasUploaded && prediction.class && (
         <View>
-          <Text style={styles.result} onPress={copyToClipboard}>
-            <Image
-              source={{ uri: image }}
-              style={{ width: 250, height: 250 }}
-            />
+          <Text style={styles.class.label}>
+            Classe: <Text style={styles.class.value}>{prediction.class}</Text>
           </Text>
-          <Text style={styles.info}>Long press to copy the image url</Text>
+          <Text style={styles.class.label}>
+            Confiança:{" "}
+            <Text style={styles.class.value}>{prediction.confidence}</Text>
+          </Text>
         </View>
       )}
 
-      <Button onPress={pickImage} title="Pick an image from camera roll" />
-      <Button onPress={takePhoto} title="Take a photo" />
-      {hasUploaded && <Button onPress={predict} title="Predict" />}
-      {hasUploaded && (
-        <View>
-          <Text>Cat: {prediction.cat}</Text>
-          <Text>Dog: {prediction.dog}</Text>
-        </View>
-      )}
+      {/* {hasUploaded && (
+        <TouchableOpacity style={styles.predictButton} onPress={predict}>
+          <Text style={styles.predictButtonLabel}>Avaliar</Text>
+        </TouchableOpacity>
+      )} */}
+      <TouchableOpacity
+        style={
+          !hasUploaded && percentage > 0
+            ? styles.disabledButton.container
+            : styles.galeryButton.container
+        }
+        onPress={pickImage}
+        disabled={!hasUploaded && percentage > 0}
+      >
+        <Text
+          style={
+            !hasUploaded && percentage > 0
+              ? styles.disabledButton.label
+              : styles.galeryButton.label
+          }
+        >
+          Selecione uma foto da galeria
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={takePhoto}
+        disabled={true}
+        style={
+          !hasUploaded && percentage > 0
+            ? styles.disabledButton.container
+            : styles.galeryButton.container
+        }
+      >
+        <Text
+          style={
+            !hasUploaded && percentage > 0
+              ? styles.disabledButton.label
+              : styles.galeryButton.label
+          }
+        >
+          Tirar uma foto
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  disabledButton: {
+    container: {
+      backgroundColor: "#ddd",
+      padding: 16,
+      borderRadius: 8,
+      marginBottom: 16,
+    },
+    label: {
+      fontSize: 16,
+    },
+  },
+  predictButton: {
+    backgroundColor: "#39743B",
+    padding: 16,
+    borderRadius: 8,
+    marginBottom: 16,
+  },
+  predictButtonLabel: { fontSize: 16, color: "#F5FCFF" },
+  galeryButton: {
+    container: {
+      backgroundColor: "#346936",
+      padding: 16,
+      borderRadius: 8,
+      marginBottom: 16,
+    },
+    label: { fontSize: 16, color: "#F5FCFF" },
+  },
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#F5FCFF",
+    paddingHorizontal: 20,
   },
   title: {
     fontSize: 20,
@@ -191,5 +329,15 @@ const styles = StyleSheet.create({
   info: {
     textAlign: "center",
     marginBottom: 20,
+  },
+  class: {
+    label: {
+      fontSize: 16,
+      marginBottom: 8,
+    },
+    value: {
+      fontWeight: "bold",
+      fontSize: 16,
+    },
   },
 });
